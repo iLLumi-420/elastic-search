816cebb263fe74ac9f7d39982eeea285df1c0a50
Sandeep Parajuli <sandeepparajuli96@gmail.com>
2024-03-06 12:10:58 +0545

    first

---
+++ elastic.py
@@ -0,0 +1,346 @@
+from elasticsearch import Elasticsearch
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+import numpy as np
+import pandas as pd
+
+
+disable_warnings(InsecureRequestWarning)
+
+es = Elasticsearch(['https://superadmin:superadmin@localhost:9200'], verify_certs=False)
+
+
+def list_indices():
+
+    res = es.cat.indices(format="json")
+
+    index_names = [index['index'] for index in res]
+
+    return index_names
+
+indices = list_indices()
+for index in indices:
+    print(index)
+    es.indices.delete(index=index)
+
+
+# id1_doc1 = {
+#     'author': 'Sandeep Parajuli',
+#     'text': 'Elasticsearch'
+# }
+# id1_doc2 = {
+#     'author2': "2",
+#     'text': 'Elasticsearch'
+# }
+
+# id2_doc1 = {
+#     'test' : 'new index',
+#     'text': 'Elasticsearch'
+# }
+
+# es.index(index='test-index1', id=1, body=id1_doc1, refresh=True)
+# es.index(index='test-index1', id=2, body=id1_doc2, refresh=True)
+# es.index(index='test-index2', id=1, body=id2_doc1, refresh=True)
+
+
+
+
+
+
+def count_documents(index_name):
+
+    count_query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+
+    res = es.count(index=index_name, body=count_query)
+
+    document_count = res['count']
+
+    print(f"Number of documents in index '{index_name}': {document_count}")
+
+
+
+def get_index_contents(index):
+    search_query = {
+        'query':{
+            "match_all": {}
+        }
+    }
+    res = es.search(index=index, body=search_query)
+
+    data = [{each['_id']: each['_source']} for each in res['hits']['hits']]
+
+    print(data)
+
+
+def search_term(index, search_term):
+    search_query = {
+        "query": {
+            "match": {
+                "text": search_term
+            }
+        }
+    }
+
+    res = es.search(index=index, body=search_query )
+    sources = [hit['_source'] for hit in res['hits']['hits']]
+
+    print(sources)
+
+
+def delete_by_id(index_name, doc_id):
+
+    print('before deleting')
+    get_index_contents(index_name)
+
+    res = es.delete(index=index_name, id=doc_id, refresh=True)
+
+    print('after deleting')
+
+    get_index_contents(index_name)
+
+
+
+def match_query(index_name, field_name, query_text):
+
+    match_query = {
+        "query": {
+            "match": {
+                field_name: query_text
+            }
+        }
+    }
+
+    res = es.search(index=index_name, body=match_query)
+
+    for hit in res['hits']['hits']:
+        print(hit['_source'])
+
+def fuzzy_search(index_name, field_name, query, max_expansions):
+
+    body = {
+        "query": {
+            "fuzzy": {
+                field_name: {
+                    "value": query,
+                    "fuzziness": "AUTO",
+                    "max_expansions": max_expansions
+                }
+            }
+        }
+    }
+
+    results = es.search(index=index_name, body=body)
+
+    if 'hits' in results and 'hits' in results['hits']:
+        return [{hit['_id']: hit['_source']} for hit in results['hits']['hits']]
+    
+    return []
+    
+
+
+def match_results(filed_name, value):
+
+    query = {
+        "query": {
+            "match": {
+                filed_name: value
+            }
+        }
+    }
+    
+    
+    res = es.search(index='school_index', body=query)
+
+    return [{each['_id']: each['_source']} for each in res['hits']['hits']]
+
+def perform_bool_search(index_name):
+
+    bool_query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"level": "Primary"}},
+                    {"match": {"type": "Public"}}
+                ]
+            }
+        }
+    }
+
+    try:
+
+        res = es.search(index=index_name, body=bool_query)
+        return [{each['_id']: each['_source']} for each in res['hits']['hits']]
+    except Exception as e:
+        print(f"An error occurred during the search: {e}")
+        return None
+
+def perform_range_search(index_name, field, min_value):
+  
+    try:
+        # Define the range query
+        range_query = {
+            "query": {
+                "range": {
+                    field: {
+                        "gt": min_value
+                    }
+                }
+            }
+        }
+
+        res = es.search(index=index_name, body=range_query)
+        return [{each['_id']: each['_source']} for each in res['hits']['hits']]
+    except Exception as e:
+        print(f"An error occurred during the search: {e}")
+        return None
+    
+def extract_region(loc_id):
+    loc_id_str = str(loc_id)
+    province_code = loc_id_str[:1]
+    district_code = loc_id_str[:3]
+    municipality_code = loc_id_str[3:5]
+    ward_number = loc_id_str[5:]
+    return {
+        "province_code": province_code,
+        "district_code": district_code,
+        "municipality_code": municipality_code,
+        "ward_number": ward_number
+    }
+
+def clean_contact_numbers(contact_numbers):
+    if contact_numbers is None:
+        return None
+    else:
+        numbers = [num.strip() for num in contact_numbers.split(',') if num.strip().isdigit()]
+        return numbers if numbers else None
+    
+def index_csv_data_to_es(index_name):
+
+    school_path = "../complete_data/updated_school_data.csv"
+    location_path = "../complete_data/location_data.csv"
+    students_count = "../complete_data/students_number.csv"
+
+    school_data = pd.read_csv(school_path)
+    location_data = pd.read_csv(location_path)
+    students_data = pd.read_csv(students_count)
+
+    school_data = school_data.iloc[200:350]
+    # location_data = location_data.iloc[:100]
+    # students_data = students_data.iloc[:100]
+
+    location_data.replace(np.nan, None, inplace=True)    
+    students_data.replace(np.nan, None, inplace=True)   
+    school_data.replace(np.nan, None, inplace=True)    
+
+
+    school_data['status'] = school_data['status'].apply(lambda x: True if x==1 else False)
+    school_data['contact_numbers'] = school_data['contact_numbers'].apply(clean_contact_numbers)
+
+    school_data.rename(columns={
+        'loc_id' : 'region'
+    }, inplace=True)
+
+    students_count_grouped = students_data.groupby('school_code').apply(lambda x:
+        {
+            f'{row["year"]}': {
+            'year': row['year'],
+            'year_ad': row['year_ad'],
+            'ecd': row['ecd_total'],
+            '1-5': row['g1_5_total'],
+            '6-8': row['g6_8_total'],
+            '9-10': row['g9_10_total'],
+            '11-12': row['g11_12_total'],
+            'total': row['total']
+        } for i, (_, row) in enumerate(x[['year', 'year_ad','ecd_total', 'g1_5_total', 'g6_8_total', 'g9_10_total', 'g11_12_total', 'total']].iterrows())
+        }).reset_index(name='students_count')
+
+    school_data = pd.merge(school_data, students_count_grouped, on='school_code', how='left')
+
+    school_data.replace(np.nan, None, inplace=True)    
+
+
+
+    
+    mappings = {
+        "mappings": {
+            "properties": {
+                "school_code": {"type": "keyword"},
+                "school_name": {"type": "text"},
+                "status": {"type": "boolean"},
+                "type": {"type": "keyword"},
+                "level": {"type": "keyword"},
+                "latest_year": {"type": "integer"},
+                "latest_year_ad": {"type": "integer"},
+                "location": {"type": "keyword"},
+                "estd_date": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd" 
+                },
+                "estd_date_ad": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd" 
+                },
+                "merged_with": {"type": "text"},
+                "head_teacher_name": {"type": "text"},
+                "email": {"type": "keyword"},
+                "contact_person": {"type": "text"},
+                "contact_numbers": {"type": "keyword"},
+                "region": {"type": "text"},
+                "students_count": {
+                    "properties": {
+                        "year": {"type": "integer"},
+                        "year_ad": {"type": "integer"},
+                        "ecd": {"type": "integer"},
+                        "1-5": {"type": "integer"},
+                        "6-8": {"type": "integer"},
+                        "9-10": {"type": "integer"},
+                        "11-12": {"type": "integer"},
+                        "total": {"type": "integer"}
+                    }
+                }
+            }
+        }
+    }
+
+    es.indices.create(index='test', body=mappings)
+
+    
+
+    for idx, row in school_data.iterrows():
+        es.index(index=index_name, id=idx, body=row.to_dict())
+
+
+if __name__ == '__main__':
+
+
+    # indices = list_indices()
+
+    # index_name = "school_index"
+    index_csv_data_to_es('test')
+
+
+    # get_index_contents(index_name)
+
+    # count_documents(index_name)
+
+    # delete_by_id(index_name, 0)
+
+    # get_index_contents(index_name)
+
+    # print(match_results('level', 'Primary'))
+
+    # print(perform_bool_search(index_name))
+
+    # print(perform_range_search(index_name, "latest_year_ad", 2021))
+
+
+    
+    
+
+
+
+
---
+++ main.py
@@ -0,0 +1,119 @@
+from elasticsearch import Elasticsearch
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+from fastapi import FastAPI, Query
+from typing import Optional
+from elasticsearch_dsl import Q
+from datetime import datetime
+
+
+
+disable_warnings(InsecureRequestWarning)
+es = Elasticsearch(['https://superadmin:superadmin@localhost:9200'], verify_certs=False)
+
+app = FastAPI()
+
+
+@app.get("/all_data")
+async def get_all_data():
+    res = es.search(index='test', body={"query": {"match_all": {}}})
+    data = [hit["_source"] for hit in res["hits"]["hits"]]
+    return data
+
+def append_function(query, append_term):
+    query['bool']['must'].append(append_term)
+
+
+@app.get("/schools/")
+async def get_schools(
+    school_code: Optional[str] = Query(None),
+    school_name: Optional[str] = Query(None),
+    status: Optional[bool] = Query(None),
+    school_type: Optional[str] = Query(None),
+    level: Optional[str] = Query(None),
+    max_latest_year: Optional[int] = Query(None),
+    min_latest_year: Optional[int] = Query(None),
+    district_code: Optional[str] = Query(None),
+    mun_code: Optional[str] = Query(None),
+    location: Optional[str] = Query(None),
+    merged_with: Optional[str] = Query(None),
+    head_teacher_name: Optional[str] = Query(None),
+    email: Optional[str] = Query(None),
+    contact_person: Optional[str] = Query(None),
+    contact_number: Optional[str] = Query(None),
+    year: Optional[str] = Query(None),
+    min_students: Optional[int] = Query(None),
+    max_students: Optional[int] = Query(None),
+    estd_date: Optional[str] = Query(None),
+    min_estd_date: Optional[str] = Query(None),
+    max_estd_date: Optional[str] = Query(None),
+):
+    query = {
+        "bool": {
+            "must": []
+        }
+    }
+
+    if school_code:
+        append_function(query, {"term": {"school_code": school_code}})
+    if school_name:
+        append_function(query, {"match": {"school_name": school_name}})
+    if status is not None:
+        append_function(query, {"term": {"status": status}})
+    if school_type:
+        append_function(query, {"term": {"type": school_type}})
+    if level:
+        append_function(query, {"term": {"level": level}})
+    if min_latest_year is not None or max_latest_year is not None:
+        append_function(query, {"range": {"latest_year": {"gte": min_latest_year or 0, "lte": max_latest_year or 2081}}})
+    if merged_with:
+        append_function(query, {"match": {"merged_with": merged_with}})
+    if location:
+        append_function(query, {"term": {"location": location}})
+    if head_teacher_name:
+        append_function(query, {"match": {"head_teacher_name": head_teacher_name}})
+    if email:
+        append_function(query, {"term": {"email": email}})
+    if contact_person:
+        append_function(query, {"match": {"contact_person": contact_person}})
+    if contact_number:
+        append_function(query, {"term": {"contact_numbers": contact_number}})
+    if year and (min_students is not None or max_students is not None):            
+        append_function(query, {"range": {f"students_count.{year}.total": {"gte": min_students or 0, "lte": max_students or 100}}})
+    if district_code is not None and mun_code is not None:
+        append_function(query, {"wildcard": {"region": f'{district_code}{mun_code}*'}})
+    if district_code is not None:
+        append_function(query, {"wildcard": {"region": f'{district_code}*'}})
+    if estd_date:
+        query["bool"]["must"].append({"match": {"estd_date": estd_date}})
+    if min_estd_date is not None or max_estd_date is not None:
+        append_function(query, {"range": {"estd_year": {"gte": min_estd_date or 0, "lte": max_estd_date or 2081}}})
+
+
+
+        
+    res = es.search(index='test', body={"query": query})
+    data = [hit["_source"] for hit in res["hits"]["hits"]]
+    return data
+
+@app.get('/test')
+def test():
+    query = {
+        "query": {
+                    "range": {
+                        "students_count.2079.total": {
+                            "gt": 5
+                            }
+                        }
+                    }
+        }
+    res = es.search(index='test', body=query)
+    data = [hit["_source"] for hit in res["hits"]["hits"]]
+    return data
+
+
+
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
